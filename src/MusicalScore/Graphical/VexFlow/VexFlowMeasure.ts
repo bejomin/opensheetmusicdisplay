417a6c4acd002f890448bd42921e92a6b204fa34
Ben Newton <bejomin@me.com>
2026-09-24 08:31:12 +0100

    Restore native volta rendering

--- src/MusicalScore/Graphical/VexFlow/VexFlowMeasure.ts
+++ src/MusicalScore/Graphical/VexFlow/VexFlowMeasure.ts
@@ -44,7 +44,6 @@ import {
     resolveFingeringPlacement,
 } from "../FingeringPlacement";
 import { VexFlowFingeringModifier } from "./VexFlowFingeringModifier";
-import { VexFlowFullWidthVolta } from "./VexFlowFullWidthVolta";
 
 const DOUBLE_HEAVY_BARLINE_TYPE: number = 8;
 
@@ -801,9 +800,7 @@ export class VexFlowMeasure extends GraphicalMeasure {
             vexFlowVoltaHeight *= 10;
             const voltaLabel: string = formatVoltaLabel(repetitionInstruction.endingIndices);
             // VexFlow's runtime Volta modifier accepts string labels, but the vendored type definition still narrows this to number.
-            this.stave.addModifier(new VexFlowFullWidthVolta(
-                voltaType, voltaLabel, this.stave.getX(), vexFlowVoltaHeight,
-            ));
+            this.stave.setVoltaType(voltaType, voltaLabel, vexFlowVoltaHeight);
             const modifiers: FontAssignableModifier[] =
                 ((this.stave as any).modifiers as FontAssignableModifier[]) || [];
             const voltaModifier: FontAssignableModifier | undefined = [...modifiers]
